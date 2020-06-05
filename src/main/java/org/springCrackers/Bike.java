35130e145f9b8e67550c4d96c8ba4de44e634db6
naglec13 <naglec13@gmail.com>
2020-06-06 00:13:49 +0300

    ..fix the problem with ApplicationContext - the default package for configuration files for Maven is src.main.resources - in another folders Maven couldn't find any context by default

--- src/main/java/org/springCrackers/Bike.java
+++ src/main/java/org/springCrackers/Bike.java
@@ -3,6 +3,6 @@ package org.springCrackers;
 public class Bike implements Vehicle {
     @Override
     public void drive() {
-        System.out.println("You're driving your car");
+        System.out.println("You're driving your bike");
     }
 }
