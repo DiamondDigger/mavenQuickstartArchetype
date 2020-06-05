723b781e69cbe5e90403840a7b5c552dc68539f3
naglec13 <naglec13@gmail.com>
2020-06-05 18:01:34 +0300

    ..add spring-context dependency in pom.xml file for using ApplicationContext in App.class

--- src/main/java/org/springCrackers/App.java
+++ src/main/java/org/springCrackers/App.java
@@ -1,17 +1,18 @@
 package org.springCrackers;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Car car = new Car();
-        car.drive();
+public class App {
+    public static void main(String[] args) {
 
-        Bike bike = new Bike();
-        bike.drive();
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+
+        Vehicle obj = getBean("vehicle");
+        obj.drive();
+
     }
 }
