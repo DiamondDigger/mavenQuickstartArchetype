1e321aa94c4e2cb4b5957a0178c5f791911b745b
naglec13 <naglec13@gmail.com>
2020-06-05 18:42:24 +0300

    ..add another spring.xml file for context configuration our beans

--- src/main/java/org/springCrackers/App.java
+++ src/main/java/org/springCrackers/App.java
@@ -9,9 +9,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-        Vehicle obj = getBean("vehicle");
+        Vehicle obj = (Vehicle) context.getBean("vehicle");
         obj.drive();
 
     }
